b022ed8fc32bed2101c14c87a3ca74e788cee91d
Jacqueline Liu <liu.jacqueline@berkeley.edu>
2016-10-27 16:58:07 -0700

    Preprocessing: made vectors of indices for train and test sets

--- code/scripts/data-preprocessing.R
+++ code/scripts/data-preprocessing.R
@@ -1,3 +1,5 @@
+set.seed(123)
+
 # Read and format data into numerical form
 credit.data <- read.csv("data/Credit.csv")
 credit.data$X <- NULL
@@ -7,5 +9,8 @@ credit.data <- cbind(data.frame(model.matrix(Balance~., data=credit.data))[-1],
 # Centering and standardizing the features
 scaled.data <- scale(credit.data)
 
-# Save as csv file
+# Save dataset csv fil and training/testing set vectors
 write.csv(scaled.data, file="data/scaled-credit.csv")
+train <- sample(1:400, 300)
+test <- (1:400)[-train]
+save(train, test, file="data/test-train-vectors.csv")
